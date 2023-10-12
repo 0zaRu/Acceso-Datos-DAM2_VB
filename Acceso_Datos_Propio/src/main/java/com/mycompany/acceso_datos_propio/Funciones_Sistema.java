e055862aa8021294350b00a6d71dd1c03bb721bf
OzaRu <114357633+0zaRu@users.noreply.github.com>
2023-10-12 19:38:00 +0200

    commit

--- Acceso_Datos_Propio/src/main/java/com/mycompany/acceso_datos_propio/Funciones_Sistema.java
+++ Acceso_Datos_Propio/src/main/java/com/mycompany/acceso_datos_propio/Funciones_Sistema.java
@@ -11,5 +11,16 @@ package com.mycompany.acceso_datos_propio;
 public class Funciones_Sistema {
     public static void main(String[] args){
         
+        //Diferentes system properties en:
+        //https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
+
+        System.out.println("\n\nEl usuario que está en ejecución es: " + System.getProperty("user.home"));
+        System.out.println("  \nEl separador del sistema es: " + System.getProperty("file.separator"));
+        System.out.println("  \nActualmente la ruta donde está ejecutando el programa es: "+ System.getProperty("user.dir"));
+        
+        //Este no se muestra porque escribe separador de línea, vamos como pner \n
+        System.out.println("  \nLas líneas se separan con el caracter: " + System.getProperty("line.separator"));
+
+        
     }
 }
---
+++ Acceso_Datos_Propio/src/main/java/com/mycompany/acceso_datos_propio/IOTextFiles.java
@@ -0,0 +1,55 @@
+package com.mycompany.acceso_datos_propio;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class IOTextFiles {
+
+    static Scanner kb = new Scanner(System.in);
+    public static void main(String[] args){
+        
+        kb.useDelimiter(System.getProperty("line.separator"));
+
+        System.out.println("Escribe una frase para almacenar en un fichero:");
+        String frase = kb.nextLine();
+
+        File ruta = new File(System.getProperty("user.dir")+System.getProperty("file.separator")+"archivos");
+        if(!ruta.exists())
+            ruta.mkdir();
+
+
+        escribeFrase(frase, ruta);
+
+    }
+
+
+    private static void escribeFrase(String frase, File ruta) {
+        File archivo = new File(ruta, System.getProperty("file.separator")+"archivo.txt");
+
+        FileWriter fichero = null;
+        PrintWriter escritor = null;
+
+        try{
+            if(archivo.exists())
+                escritor = new PrintWriter(fichero = new FileWriter(archivo, true), true);
+            else
+                escritor = new PrintWriter(fichero = new FileWriter(archivo, true));
+
+            escritor.println(frase);
+        
+        }catch(IOException e){
+            e.getMessage();
+        
+        }finally{
+            try {
+                escritor.close();
+                fichero.close();
+            } catch (IOException e) {
+                e.getMessage();
+            }
+        }
+    }
+}
